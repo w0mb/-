320302b4fd242b226519f72056a240c302cc1e39
w0mb <ip217s14@csc.sibsutis.ru>
2024-02-05 21:09:37 +0700

    pogresnost` done

--- labs/laba1/main.cpp
+++ labs/laba1/main.cpp
@@ -3,6 +3,57 @@
 #include <cmath>
 
 using namespace std;
+struct NumberWithUncertainty {
+    double value;
+    double uncertainty;
+
+    // Конструктор для создания числа с погрешностью
+    NumberWithUncertainty(double val, double unc) : value(val), uncertainty(unc) {}
+    NumberWithUncertainty(){}
+};
+
+NumberWithUncertainty calculateWithUncertainty(const NumberWithUncertainty &a, const NumberWithUncertainty &b, char operation) {
+    // Пример вычисления с погрешностью для операций +, -, *, /
+    NumberWithUncertainty result;
+
+    switch (operation) {
+        case '+':
+            result.value = a.value + b.value;
+            result.uncertainty = sqrt(pow(a.uncertainty, 2) + pow(b.uncertainty, 2));
+            break;
+        case '-':
+            result.value = a.value - b.value;
+            result.uncertainty = sqrt(pow(a.uncertainty, 2) + pow(b.uncertainty, 2));
+            break;
+        case '*':
+            result.value = a.value * b.value;
+            result.uncertainty = fabs(result.value) * sqrt(pow(a.uncertainty / a.value, 2) + pow(b.uncertainty / b.value, 2));
+            break;
+        case '/':
+            if (b.value != 0) {
+                result.value = a.value / b.value;
+                result.uncertainty = fabs(result.value) * sqrt(pow(a.uncertainty / (a.value * a.value), 2) +
+                                                               pow(b.uncertainty / (b.value * b.value), 2));
+            } else {
+                // Обработка деления на ноль
+                cerr << "Error: Division by zero." << endl;
+                result.value = NAN;
+                result.uncertainty = NAN;
+            }
+            break;
+        default:
+            cerr << "Error: Unsupported operation." << endl;
+            result.value = NAN;
+            result.uncertainty = NAN;
+            break;
+    }
+
+    return result;
+}
+
+void printNumberWithUncertainty(const NumberWithUncertainty &number) {
+    cout << number.value << " +/- " << number.uncertainty;
+}
 
 // Function to perform Gaussian elimination
 void gaussianElimination(vector<vector<double>> &matrix)
@@ -104,6 +155,34 @@ int main()
     printSolution(solution);
     cout << endl;
     printSolution(coefficients);
+
+//--------------------------------------------------------------------------------------------------------//
+    NumberWithUncertainty a(2.384, 0.021);
+    NumberWithUncertainty b(9.485, 0.014);
+
+    // Вычисление с погрешностью для операций +, -, *, /
+    NumberWithUncertainty resultAdd = calculateWithUncertainty(a, b, '+');
+    NumberWithUncertainty resultSubtract = calculateWithUncertainty(a, b, '-');
+    NumberWithUncertainty resultMultiply = calculateWithUncertainty(a, b, '*');
+    NumberWithUncertainty resultDivide = calculateWithUncertainty(a, b, '/');
+
+    // Вывод результатов
+    cout << "Addition: ";
+    printNumberWithUncertainty(resultAdd);
+    cout << endl;
+
+    cout << "Subtraction: ";
+    printNumberWithUncertainty(resultSubtract);
+    cout << endl;
+
+    cout << "Multiplication: ";
+    printNumberWithUncertainty(resultMultiply);
+    cout << endl;
+
+    cout << "Division: ";
+    printNumberWithUncertainty(resultDivide);
+    cout << endl;
+
 
     return 0;
 }
