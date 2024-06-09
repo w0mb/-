dca5a72c16b9f949ff0ce7b53a9ff8908d2c18b9
w0mb <ip217s14@csc.sibsutis.ru>
2024-06-09 19:59:09 +0700

    dfdf

---
+++ tempCodeRunnerFile.cpp
@@ -0,0 +1,5 @@
+    // cout << "========================== Lagrange Interpolation ==========================" << endl;
+    // for (double x : interpolation_points) {
+    //     double y_interpolated = lagrange(x_arr, y_arr, x);
+    //     cout << "interpolation in x = " << x << " : y = " << y_interpolated << endl;
+    // }
